4800a6db8ee968c748c890f72dfaf6a421385b37
Astariul <remondnicola@gmail.com>
2021-10-04 15:48:41 +0900

    :sparkles: Make cannon follow the mouse

--- src/utils/formulas.js
+++ src/utils/formulas.js
@@ -1,3 +1,5 @@
+export const radiansToDegrees = radians => ((radians * 180) / Math.PI);
+
 export const pathFromBezierCurve = (cubicBezierCurve) => {
     const {
         initialAxis, initialControlPoint, endingControlPoint, endingAxis,
@@ -8,4 +10,31 @@ export const pathFromBezierCurve = (cubicBezierCurve) => {
         ${endingControlPoint.x} ${endingControlPoint.y}
         ${endingAxis.x} ${endingAxis.y}
     `;
+};
+
+// https://math.stackexchange.com/questions/714378/find-the-angle-that-creating-with-y-axis-in-degrees
+export const calculateAngle = (x1, y1, x2, y2) => {
+    if (x2 >= 0 && y2 >= 0) {
+        return 90;
+    } else if (x2 < 0 && y2 >= 0) {
+        return -90;
+    }
+
+    const dividend = x2 - x1;
+    const divisor = y2 - y1;
+    const quotient = dividend / divisor;
+    return radiansToDegrees(Math.atan(quotient)) * -1;
+};
+
+export const getCanvasPosition = (event) => {
+    // mouse position on auto-scaling canvas
+    // https://stackoverflow.com/a/10298843/1232793
+  
+    const svg = document.getElementById('aliens-go-home-canvas');
+    const point = svg.createSVGPoint();
+  
+    point.x = event.clientX;
+    point.y = event.clientY;
+    const { x, y } = point.matrixTransform(svg.getScreenCTM().inverse());
+    return {x, y};
   };
