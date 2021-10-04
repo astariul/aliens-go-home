68df88acd8d3955ddd3c0eb23cbb6819f92d2215
Astariul <remondnicola@gmail.com>
2021-10-04 15:10:05 +0900

    :sparkles: Add redux
    And fix bug due to the age of the tutorial

--- src/index.js
+++ src/index.js
@@ -1,14 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
 import './index.css';
-import App from './App';
+import Game from './containers/Game';
+import reducer from './reducers';
 import reportWebVitals from './reportWebVitals';
 
+/* eslint-disable no-underscore-dangle */
+const store = createStore(
+    reducer, /* preloadedState, */
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
+);
+/* eslint-enable */
+
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <Provider store={store}>
+        <Game />
+    </Provider>,
+    document.getElementById('root'),
 );
 
 // If you want to start measuring performance in your app, pass a function
