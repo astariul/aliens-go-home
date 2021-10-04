a3963df4910fd75c63cc655ffedca09504539bc9
Astariul <remondnicola@gmail.com>
2021-10-04 15:26:22 +0900

    :sparkles: Add ground and sky

--- src/App.js
+++ src/App.js
@@ -1,18 +1,12 @@
 import React, {Component} from 'react';
-import PropTypes from 'prop-types'      ;
+import Canvas from './components/Canvas';
 
 class App extends Component {
     render() {
         return (
-            <div className="App">
-                <h1>{this.props.message}</h1>
-            </div>
+            <Canvas />
         );
     }
 }
-
-App.propTypes = {
-  message: PropTypes.string.isRequired,
-};
 
 export default App;
